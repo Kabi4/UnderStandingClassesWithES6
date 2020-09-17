ca454217824303061d3d6ee652fe9671350a9079
Kushagra Singh <59528754+Kabi4@users.noreply.github.com>
2020-09-17 19:31:37 +0530

    Update index.js

--- Simple Counter REDUX-REACT/src/index.js
+++ Simple Counter REDUX-REACT/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import {createStore,applyMiddleware, compose} from 'redux';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker'; 
 import reducer from './Store/reducer';
 import {Provider} from 'react-redux';
 import thunk from 'redux-thunk';
